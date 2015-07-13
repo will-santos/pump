39e123ec4a7569c5e1314129ac80e316c277c412
will <w.santos.python@gmail.com>
2015-07-13 19:43:09 -0300

    mudando para config com classes

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
-DEBUG = True
+class BaseConfig:
+
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = 'melhor isso aqui depois =D'
+
+
+class Producao(BaseConfig):
+
+    SECRET_KEY = 'aqui deve ser melhorado ainda mais - de um arquivo de fora.'
+    DEBUG = False
+
+
+class Desenvolvimento(BaseConfig):
+
+    TESTING = True
--- pump/__init__.py
+++ pump/__init__.py
@@ -4,5 +4,5 @@ from flask import Flask
 from .views.home import home
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('config.Desenvolvimento')
 app.register_blueprint(home)
